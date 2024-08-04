82386bc7175188a5d3c8322b33104efa5a719c5f
Maksim <qwertyclop@gmail.com>
2024-08-04 20:21:08 +0600

    ✅ Админка ✅ Вывод статей ✅ Просмотр статей ✅ Удаление статей ✅ Создание статей ✅ Комментирование статей ✅ Удаление статей, если ты админ

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,36 @@
 from django import forms
-from .models import Public
+from .models import Public, Category, Comment
 
 
 class CreatePublicForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=Category.objects.all(),widget=forms.Select)
+
     class Meta:
         model = Public
-        fields = '__all__'
+        fields = ['title', 'text', 'teg', 'category']
 
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+
+# class RedactorPublicForm(forms.ModelForm):
+#     title = forms.CharField(max_length=55)
+#     text = forms.TextInput()
+#     teg = forms.TextInput()
+#
+#     class Meta:
+#         model = Public
+#         fields = ['title', 'text', 'teg']
+#
+#     def save(self, commit=True):
+#         public = Public.objects.create_public(
+#             title=self.cleaned_data['title'],
+#             text=self.cleaned_data['text'],
+#             teg=self.cleaned_data['tag']
+#         )
+#         result = Public(public)
+#         if commit:
+#             result.save()
+#         return result
--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('publics/', views.ViewPublic.as_view(), name='allpublic'),
-    path('create/', views.create_public, name='create'),
-
+    path('public/', views.ViewPublic.as_view(), name='allpublic'),
+    path('public/create/', views.create_public, name='create'),
+    path('public/<int:pk>/', views.DetailViewPublic.as_view(), name='detail'),
+    path('public/<int:pk>/edit/', views.RedactorPublic.as_view(),
+         name='public_edit'),
+    path('delete/', views.delete_public, name='delete'),
+    path('delete_comment/', views.delete_comment, name='delete_comment')
 ]
